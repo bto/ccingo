bb6a5de2e307b4779b07484b4f959528e93dafd9
Masato Bito <masato@bz2.jp>
2019-04-19 23:17:18 +0900

    add 03 test

---
+++ sample/03_num_test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+SRC_DIR=$(dirname $0)
+TOP_DIR=$SRC_DIR/..
+BUILD_DIR=$TOP_DIR/build
+
+GO_FILE=$SRC_DIR/03_num.go
+AS_FILE=$BUILD_DIR/03_num.s
+EXEC_FILE=$BUILD_DIR/03_num
+
+function try() {
+    expected=$1
+    input=$2
+
+    echo $input | go run $GO_FILE > $AS_FILE
+    gcc $AS_FILE -o $EXEC_FILE
+    $EXEC_FILE
+    ret=$?
+
+    if [ $ret = $expected ]; then
+        echo "ok: $input => $expected"
+    else
+        echo "NOT OK: $input => $expected"
+    fi
+}
+
+echo $(basename $0)
+try 1 1
+try 2 2
