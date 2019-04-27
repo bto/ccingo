fd2a788928a87b274a57a2cf9843890025188861
Masato Bito <masato@bz2.jp>
2019-04-28 06:50:37 +0900

    divide

---
+++ practice/04_syntax/01_addsub/cc/asm.go
@@ -0,0 +1,38 @@
+package cc
+
+import (
+	"fmt"
+)
+
+func PrintAsm(nd *node) {
+	fmt.Println(".intel_syntax noprefix")
+	fmt.Println(".global main")
+	fmt.Println("main:")
+
+	gen(nd)
+
+	fmt.Println("  pop rax")
+	fmt.Println("  ret")
+}
+
+func gen(nd *node) {
+	if nd.ty == ND_NUM {
+		fmt.Println("  push", nd.val)
+		return
+	}
+
+	gen(nd.lhs)
+	gen(nd.rhs)
+
+	fmt.Println("  pop rdi")
+	fmt.Println("  pop rax")
+
+	switch nd.ty {
+	case '+':
+		fmt.Println("  add rax, rdi")
+	case '-':
+		fmt.Println("  sub rax, rdi")
+	}
+
+	fmt.Println("  push rax")
+}
---
+++ practice/04_syntax/01_addsub/cc/parse.go
@@ -0,0 +1,59 @@
+package cc
+
+import (
+	"log"
+)
+
+const (
+	ND_NUM = iota + 256
+)
+
+type node struct {
+	ty, val  int
+	lhs, rhs *node
+}
+
+func Parse(tks *tokens) *node {
+	return add(tks)
+}
+
+func add(tks *tokens) *node {
+	nd := num(tks)
+	return addx(tks, nd)
+}
+
+func addx(tks *tokens, nd *node) *node {
+	switch {
+	case tks.consume('+'):
+		ndNum := num(tks)
+		nd = &node{
+			ty:  '+',
+			lhs: nd,
+			rhs: ndNum,
+		}
+		return addx(tks, nd)
+	case tks.consume('-'):
+		ndNum := num(tks)
+		nd = &node{
+			ty:  '-',
+			lhs: nd,
+			rhs: ndNum,
+		}
+		return addx(tks, nd)
+	default:
+		return nd
+	}
+}
+
+func num(tks *tokens) *node {
+	tk := tks.current()
+	if tk.ty != TK_NUM {
+		log.Fatal("数値ではないトークンです: ", string(tk.input))
+	}
+
+	tks.next()
+	return &node{
+		ty:  ND_NUM,
+		val: tk.val,
+	}
+}
