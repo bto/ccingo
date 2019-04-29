1e1c2bda69f8b223ed39a68e8bd69929fa286f2f
Masato Bito <masato@bz2.jp>
2019-04-29 15:27:13 +0900

    init if

---
+++ practice/11_ctrl/01_if/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"bufio"
+	"os"
+
+	"github.com/bto/ccingo/practice/10_var/cc"
+)
+
+func main() {
+	rd := bufio.NewReader(os.Stdin)
+	tks := cc.Tokenize(rd)
+	nds := cc.Parse(tks)
+	cc.PrintAsm(nds)
+}
