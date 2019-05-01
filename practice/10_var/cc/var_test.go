22f44b28f28725e84f852b5b64e1d8e7b3659069
Masato Bito <masato@bz2.jp>
2019-05-01 22:42:40 +0900

    fix test

--- practice/10_var/cc/var_test.go
+++ practice/10_var/cc/var_test.go
@@ -18,40 +18,40 @@ func TestVariables(t *testing.T) {
 	if variable.offset != 8 {
 		t.Fatal("invalid offset:", variable.offset)
 	}
-	if variable.name == "foo" {
+	if variable.name != "foo" {
 		t.Fatal("invalid name:", variable.name)
 	}
 	if !vars.exist("foo") {
 		t.Fatal("return true for non-exsitent variable")
 	}
 
-	variable := vars.add("bar")
+	variable = vars.add("bar")
 	if vars.offset != 16 {
 		t.Fatal("invalid offset:", vars.offset)
 	}
 	if variable.offset != 16 {
 		t.Fatal("invalid offset:", variable.offset)
 	}
-	if variable.name == "bar" {
+	if variable.name != "bar" {
 		t.Fatal("invalid name:", variable.name)
 	}
 	if !vars.exist("bar") {
 		t.Fatal("return true for non-exsitent variable")
 	}
 
-	variable := vars.get("foo")
+	variable = vars.get("foo")
 	if variable.offset != 8 {
 		t.Fatal("invalid offset:", variable.offset)
 	}
-	if variable.name == "foo" {
+	if variable.name != "foo" {
 		t.Fatal("invalid name:", variable.name)
 	}
 
-	variable := vars.get("bar")
+	variable = vars.get("bar")
 	if variable.offset != 16 {
 		t.Fatal("invalid offset:", variable.offset)
 	}
-	if variable.name == "bar" {
+	if variable.name != "bar" {
 		t.Fatal("invalid name:", variable.name)
 	}
 }
