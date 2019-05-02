0909388ea8e7ffea24eb39ea04678e6da7cef125
Masato Bito <masato@bz2.jp>
2019-05-02 13:19:44 +0900

    fix test

--- practice/13_func/01_noarg/test.sh
+++ practice/13_func/01_noarg/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SRC_DIR=$(cd $(dirname $0); pwd)
-TOP_DIR=$(cd $SRC_DIR/../..; pwd)
-source $SRC_DIR/../common.sh
+TOP_DIR=$(cd $SRC_DIR/../../..; pwd)
+source $SRC_DIR/../../common.sh
 
 try 3 '1 + 2;'
 try 2 '  1+5
