384bfa91f2568b42aa3ffc062cedc342a95ebc31
Masato Bito <masato@bz2.jp>
2019-05-19 22:39:41 +0900

    add context struct

--- cc/llvm.go
+++ cc/llvm.go
@@ -11,17 +11,26 @@ import (
 	"github.com/llir/llvm/ir/value"
 )
 
+type context struct {
+	fn *ir.Func
+	bl *ir.Block
+	vars map[string]*ir.InstAlloca
+}
+
 func (nds nodes) PrintLlvm() {
 	var v value.Value
-	vars := make(map[string]*ir.InstAlloca)
+	cn := &context{
+		vars: make(map[string]*ir.InstAlloca),
+	}
 
 	m := ir.NewModule()
-	block := m.NewFunc("main", types.I64).NewBlock("")
+	cn.fn = m.NewFunc("main", types.I64)
+	cn.bl = cn.fn.NewBlock("")
 
 	for _, nd := range nds {
-		v = nd.gen(block, vars)
+		v = nd.gen(cn)
 		if v.Type() != types.I64 {
-			v = block.NewZExt(v, types.I64)
+			v = cn.bl.NewZExt(v, types.I64)
 		}
 
 		if nd.ty == ND_RETURN {
@@ -29,22 +38,22 @@ func (nds nodes) PrintLlvm() {
 		}
 	}
 
-	block.NewRet(v)
+	cn.bl.NewRet(v)
 	fmt.Println(m)
 }
 
-func (nd *node) gen(block *ir.Block, vars map[string]*ir.InstAlloca) value.Value {
+func (nd *node) gen(cn *context) value.Value {
 	switch nd.ty {
 	case ND_NUM:
 		return nd.genNum()
 	case ND_VAR:
-		return nd.genVar(block, vars)
+		return nd.genVar(cn)
 	case ND_RETURN:
-		return nd.genReturn(block, vars)
+		return nd.genReturn(cn)
 	case int('='):
-		return nd.genAssign(block, vars)
+		return nd.genAssign(cn)
 	default:
-		return nd.genOp(block, vars)
+		return nd.genOp(cn)
 	}
 }
 
@@ -52,74 +61,74 @@ func (nd *node) genNum() *constant.Int {
 	return constant.NewInt(types.I64, int64(nd.val))
 }
 
-func (nd *node) genVar(block *ir.Block, vars map[string]*ir.InstAlloca) value.Value {
-	r := nd.genLval(block, vars)
-	return block.NewLoad(r)
+func (nd *node) genVar(cn *context) value.Value {
+	r := nd.genLval(cn)
+	return cn.bl.NewLoad(r)
 }
 
-func (nd *node) genReturn(block *ir.Block, vars map[string]*ir.InstAlloca) value.Value {
-	v := nd.lhs.gen(block, vars)
+func (nd *node) genReturn(cn *context) value.Value {
+	v := nd.lhs.gen(cn)
 	if v.Type() != types.I64 {
-		v = block.NewZExt(v, types.I64)
+		v = cn.bl.NewZExt(v, types.I64)
 	}
 	return v
 }
 
-func (nd *node) genAssign(block *ir.Block, vars map[string]*ir.InstAlloca) value.Value {
-	r := nd.lhs.genLval(block, vars)
+func (nd *node) genAssign(cn *context) value.Value {
+	r := nd.lhs.genLval(cn)
 
-	v := nd.rhs.gen(block, vars)
+	v := nd.rhs.gen(cn)
 	if v.Type() != types.I64 {
-		v = block.NewZExt(v, types.I64)
+		v = cn.bl.NewZExt(v, types.I64)
 	}
 
-	block.NewStore(v, r)
+	cn.bl.NewStore(v, r)
 	return v
 }
 
-func (nd *node) genOp(block *ir.Block, vars map[string]*ir.InstAlloca) value.Value {
-	v1 := nd.lhs.gen(block, vars)
+func (nd *node) genOp(cn *context) value.Value {
+	v1 := nd.lhs.gen(cn)
 	if v1.Type() != types.I64 {
-		v1 = block.NewZExt(v1, types.I64)
+		v1 = cn.bl.NewZExt(v1, types.I64)
 	}
 
-	v2 := nd.rhs.gen(block, vars)
+	v2 := nd.rhs.gen(cn)
 	if v2.Type() != types.I64 {
-		v2 = block.NewZExt(v2, types.I64)
+		v2 = cn.bl.NewZExt(v2, types.I64)
 	}
 
 	switch nd.ty {
 	case '+':
-		return block.NewAdd(v1, v2)
+		return cn.bl.NewAdd(v1, v2)
 	case '-':
-		return block.NewSub(v1, v2)
+		return cn.bl.NewSub(v1, v2)
 	case '*':
-		return block.NewMul(v1, v2)
+		return cn.bl.NewMul(v1, v2)
 	case '/':
-		return block.NewUDiv(v1, v2)
+		return cn.bl.NewUDiv(v1, v2)
 	case ND_EQ:
-		return block.NewICmp(enum.IPredEQ, v1, v2)
+		return cn.bl.NewICmp(enum.IPredEQ, v1, v2)
 	case ND_NE:
-		return block.NewICmp(enum.IPredNE, v1, v2)
+		return cn.bl.NewICmp(enum.IPredNE, v1, v2)
 	case '<':
-		return block.NewICmp(enum.IPredULT, v1, v2)
+		return cn.bl.NewICmp(enum.IPredULT, v1, v2)
 	case ND_LE:
-		return block.NewICmp(enum.IPredULE, v1, v2)
+		return cn.bl.NewICmp(enum.IPredULE, v1, v2)
 	}
 
 	log.Fatal("invalid node: ", nd)
 	return nil
 }
 
-func (nd *node) genLval(block *ir.Block, vars map[string]*ir.InstAlloca) *ir.InstAlloca {
+func (nd *node) genLval(cn *context) *ir.InstAlloca {
 	if nd.ty != ND_VAR {
 		log.Fatal("left hand value is not a variable")
 	}
 
-	_, ok := vars[nd.name]
+	_, ok := cn.vars[nd.name]
 	if !ok {
-		vars[nd.name] = block.NewAlloca(types.I64)
+		cn.vars[nd.name] = cn.bl.NewAlloca(types.I64)
 	}
 
-	return vars[nd.name]
+	return cn.vars[nd.name]
 }
